69b5ec062c3dc389c75db7e0b0f8aaf1b14d4a3a
Luis Izaguirre <lsizaguirre@gmail.com>
2017-09-27 16:23:53 -0300

    Debug 22

--- messages/index.js
+++ messages/index.js
@@ -19,6 +19,11 @@ require('dotenv').config();
 const useEmulator = botUtils.getUseEmulator();
 const connector = botUtils.buildConnector(useEmulator);
 
+botUtils.buildBot(connector);
+//botUtils.startServer(useEmulator, connector);
+
+
+/*
 var bot = new builder.UniversalBot(connector);
 
 bot.localePath(path.join(__dirname, './locale'));
@@ -43,7 +48,7 @@ bot.use({
     botbuilder: middleware.LogIncomingMessage,
     send: middleware.LogOutgoingMessage
 });
-
+*/
 // Create the listening
 if (useEmulator) {
     var restify = require('restify');
