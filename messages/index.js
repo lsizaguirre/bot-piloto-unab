cf0f66683a04e024f19d464fb1a2b5120824138f
Luis Izaguirre <lsizaguirre@gmail.com>
2017-09-27 16:48:39 -0300

    Release version modularizada sin Dashbot

--- messages/index.js
+++ messages/index.js
@@ -20,41 +20,10 @@ const useEmulator = botUtils.getUseEmulator();
 const connector = botUtils.buildConnector(useEmulator);
 
 botUtils.buildBot(connector);
-//botUtils.startServer(useEmulator, connector);
 
-
-/*
-var bot = new builder.UniversalBot(connector);
-
-bot.localePath(path.join(__dirname, './locale'));
-
-bot.dialog('/Cancelar', [
-    function (session) {
-        session.endDialog('Cancelado el dialogo');
-    }
-]).triggerAction({ matches: /^cancelar/i});
-
-var recognizer = new apiairecognizer(process.env['ApiAiToken']); 
-var intents = new builder.IntentDialog({ recognizers: [recognizer] } )
-.onDefault((session, args) => {
-    var name = session.message.user ? session.message.user.name : null;
-    session.send(name + ' ' + args.entities[0].entity);
-});
-
-bot.dialog('/', intents);    
-
-// Set the Incoming and Outgoing functions for the middleware
-bot.use({
-    botbuilder: middleware.LogIncomingMessage,
-    send: middleware.LogOutgoingMessage
-});
-*/
-// Create the listening
-
-if (useEmulator) {
+if (useEmulator)
     botUtils.startLocalServer(connector); 
-} else {
+else
     module.exports = { default: connector.listen() }
-}
 
 
